c87c0bf2cb3dc223ea17ee2d369b6a30d1cf2fbd
Bruna Gomes <bruna.ckgomes@gmail.com>
2025-09-28 15:45:54 -0300

    Adicionado o hooj de beforeEach para captura do token

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -5,13 +5,15 @@ const { obterToken } = require('../helpers/autenticacao');
 
 describe('Transferências', () => {
     describe('POST /transferencias', () => {
+    let token;
+    
+        beforeEach( async () => {
+            token = await obterToken('julio.lima', '123456');
+        });
+
         it('Deve retornar sucesso com 201 quando o valor da transferência for igual ou maior que R$10,00', async () => {
         //aqui dentro do it eu faço o teste com o supertest.
-            //capturar o token
-            
-            const token = await obterToken('julio.lima', '123456');
-
-            const resposta = await request(process.env.BASE_URL)
+                const resposta = await request(process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Content-Type', 'application/json')
                 .set('Authorization', `Bearer ${token}` /*'Bearer ' + token*/)
@@ -30,8 +32,6 @@ describe('Transferências', () => {
         });
         it('Deve retornar falha com 422 quando o valor da transferência for abaixo que R$10,00', async () => {
             
-            const token = await obterToken('julio.lima', '123456');
-
             const resposta = await request(process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Content-Type', 'application/json')
